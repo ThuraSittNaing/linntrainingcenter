bf311162405255fc9b57aaddd0b7723e33616c40
ThuraSittNaing <thurasittnaing.ggwpwew@gmail.com>
2019-06-11 16:03:26 -0700

    change design make header footer common

--- admin/dashbord.php
+++ admin/dashbord.php
@@ -87,25 +87,7 @@
 
   <div id="wrapper">
 
-    <!-- Sidebar -->
-    <ul class="sidebar navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li>
-    </ul>
+    <?php include('sidebar.php') ?>
 
     <div id="content-wrapper">
 
---
+++ admin/student/form.php
@@ -0,0 +1,4 @@
+<?php 
+
+	echo " your form design go herer...";
+ ?>
