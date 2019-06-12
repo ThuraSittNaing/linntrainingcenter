a00c2c154695f084631c793a51c1880cc552ffd8
zunpwintphyu <47861883+zunpwintphyu@users.noreply.github.com>
2019-06-12 16:28:37 +0630

    fix conflict

---
+++ admin/edittable.php
@@ -0,0 +1,25 @@
+<li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>User</span></a>
+</li>
+<li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Student</span></a>
+</li>
+<li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Teacher</span></a>
+</li>
+<li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Course</span></a>
+</li>
+<li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Post</span></a>
+</li>
