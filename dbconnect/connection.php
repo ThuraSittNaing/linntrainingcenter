ebb0cfbe4b09a42c7bc6fc02f04be46182b162b8
ThuraSittNaing <50403109+ThuraSittNaing@users.noreply.github.com>
2019-06-13 20:08:35 -0700

    Revert "Feature/course module"

--- dbconnect/connection.php
+++ dbconnect/connection.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "linntraningcenter";
+$database = "linntrainingcenter";
 
 //Create Connection
 $conn = mysqli_connect($servername, $username, $password, $database);
