78fcc0e1fc75b19c51d9e37d8f9c65fec63bc0d4
thinnadizaw <50195649+thinnadizaw@users.noreply.github.com>
2019-06-20 09:18:38 +0630

    Revert "postdest"
    This reverts commit 97b2f389c81a28a981236983febb4bf3f107caa5.

--- admin/post/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../../dbconnect/connection.php";
-$id = $_GET['id'];
-$result = mysqli_query($conn, "DELETE FROM post WHERE id=$id");
-// var_dump($result);
-header("Location:  http://localhost/linntrainingcenter/admin/posts.php");
-?>
