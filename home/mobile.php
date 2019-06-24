62c43f1fd6bfd371d34022f7655e64b5fd42e687
aungsoeoo <aungsoeoo.94s@gmail.com>
2019-06-24 10:01:43 +0630

    Update mobile.php

--- home/mobile.php
+++ home/mobile.php
@@ -123,24 +123,19 @@
     </style>
 </head>
 <body>
-<<<<<<< Updated upstream
 <div class="header col-xs-12">
     <img src="../image/linnlogo.jpg" width="100px" height="90px">
     <b>Linn It Solution Co.,Ltd.</b>
     <ul>
-		  <li><a class="active" href="../index.php">Home</a></li>
-		  <li><a class="active" href="../course.php">Course</a></li>
+          <li><a class="active" href="../index.php">Home</a></li>
+          <li><a class="active" href="../course.php">Course</a></li>
           <li><a class="active" href="../news.php">News</a></li>
           <li><a class="active" href="../contact.php">Contact</a></li>
           <!-- <li><a class="active" href="../about.php">About</a></li> -->
-		  <!-- <li style="float:right"><a class="active" href="../login.php">Login</a></li> -->
-		  
-	</ul>
+          <!-- <li style="float:right"><a class="active" href="../login.php">Login</a></li> -->
+          
+    </ul>
 </div>
-=======
-
-</div><br>
->>>>>>> Stashed changes
 <div class="container ">
  <h3 style="color:#751B08; margin-bottom:30px;"><center>Mobile Service</center></h3>
  <h5 style="color:#751B08;">Software Service</h5>
